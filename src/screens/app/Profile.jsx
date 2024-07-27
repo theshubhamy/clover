50a8c1b1bde2da91e34a150eed672cdb1ddda213
shubham kumar <theshubham.dev@gmail.com>
2024-07-28 02:52:46 +0530

    updated README.md

--- src/screens/app/Profile.jsx
+++ src/screens/app/Profile.jsx
@@ -1,7 +1,6 @@
 import {View, Text, Image, TouchableOpacity, FlatList} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {MIcon} from '../../components/Icons';
 import {useAuth} from '../../context/AuthContext';
 const Profile = () => {
   const {user, signOut} = useAuth();
