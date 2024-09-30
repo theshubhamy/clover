b6f312b4345b0ace95e69b7cb04dac530acb9255
shubham kumar <theshubham.dev@gmail.com>
2024-10-01 00:54:41 +0530

    Update react native Package and pods and fixed build issue.

--- src/screens/app/Home.jsx
+++ src/screens/app/Home.jsx
@@ -2,7 +2,7 @@ import React, {useCallback} from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {useQuery} from '@tanstack/react-query';
-import SwiperCard from '../../components/SwiperCard';
+// import SwiperCard from '../../components/SwiperCard';
 import {useAuth} from '../../context/AuthContext';
 import {fetchUsers} from '../../services/userService';
 import {useLocationCtx} from '../../context/LocationContext';
@@ -15,6 +15,8 @@ const Home = ({navigation}) => {
     queryFn: () => fetchUsers(user?.uid),
     enabled: !!user?.uid,
   });
+  console.log(data);
+
   const navigateToPreferences = () => {
     navigation.navigate('Preference');
   };
@@ -50,12 +52,12 @@ const Home = ({navigation}) => {
         </View>
         {/* SwiperCard */}
 
-        <SwiperCard
+        {/* <SwiperCard
           data={data}
           navigateToPreferences={navigateToPreferences}
           handleRefresh={handleRefresh}
           isLoading={isLoading}
-        />
+        /> */}
       </View>
     </>
   );
